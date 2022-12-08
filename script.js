de0c72a1f53d86ada114677e082aba81815de3e5
Смирнов Сергей <sesmirnov@mmtr.ru>
2022-12-09 01:09:13 +0300

    Fix delete method

--- script.js
+++ script.js
@@ -58,14 +58,14 @@ app.put('/api/books/:id', (req, res) => {
 
 //DELETE Request Handler
 app.delete('/api/books/:id', (req, res) => {
-
     const book = books.find(c => c.id === parseInt(req.params.id));
-    if (!book) res.status(404).send('<h2 style="font-family: Malgun Gothic; color: darkred;"> Not Found!! </h2>');
-
-    const index = books.indexOf(book);
-    books.splice(index, 1);
-
-    res.send(book);
+    if (book == undefined) {
+        res.status(404).send('<h2 style="font-family: Malgun Gothic; color: darkred;"> Not Found!! </h2>');
+    } else {
+        const index = books.indexOf(book);
+        books.splice(index, 1);
+        res.send(book);
+    }
 });
 
 function validateBook(book) {
